13f694452becd11268e524fe73083799ca998be9
Md.Zilhaj Un Noor <zilhajsajid@gmail.com>
2025-10-24 22:30:54 +0600

    made some changes in footer

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,63 @@
-import React from "react";
+import React, { use, useState } from "react";
 import Navbar from "../components/Navbar";
-import { Link } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const { logInUser } = use(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  // console.log(location);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    logInUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        // console.log(user);
+        navigate(`${location.state ? location.state : "/"}`);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        // const errorMessage = error.message;
+        setError(errorCode);
+        // alert(errorCode, errorMessage);
+      });
+  };
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
         <h1 className="font-semibold text-2xl text-center">
           Login Your Account
         </h1>
-        <div className="card-body">
+        <form onSubmit={handleLogin} className="card-body">
           <fieldset className="fieldset">
+            {/* email */}
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+              required
+            />
+            {/* password */}
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
             <div>
               <a className="link link-hover">Forgot password?</a>
             </div>
+            {error && <p className="text-red-700 text-xs">{error}</p>}
             <p className="text-center font-semibold pt-5">
               Don’t Have An Account ?{" "}
               <Link className="text-secondary" to="/auth/register">
@@ -26,7 +66,7 @@ const Login = () => {
             </p>
             <button className="btn btn-neutral mt-4">Login</button>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,47 @@
-import React from "react";
-import { Link } from "react-router";
+import React, { use, useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const Register = () => {
+  const [nameError, setNameError] = useState("");
+  const { createUser, setUser, updateUser } = use(AuthContext);
+  const navigate = useNavigate();
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value;
+    if (name.length < 5) {
+      setNameError("Your name should be more than five characters");
+      return;
+    } else {
+      setNameError("");
+    }
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    // console.log({ name, photo, email, password });
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        // console.log(user);
+        updateUser({ displayName: name, photoURL: photo })
+          .then(() => {
+            setUser({ ...user, displayName: name, photoURL: photo });
+            navigate("/");
+          })
+          .catch((error) => {
+            console.log(error);
+            setUser(user);
+          });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        alert(errorMessage);
+      });
+  };
   return (
     <div>
       <div className="flex justify-center items-center min-h-screen">
@@ -9,33 +49,56 @@ const Register = () => {
           <h1 className="font-semibold text-2xl text-center">
             Register Your Account
           </h1>
-          <div className="card-body">
+          <form onSubmit={handleRegister} className="card-body">
             <fieldset className="fieldset">
               {/* name */}
               <label className="label">Name</label>
-              <input type="name" className="input" placeholder="Your Name" />
+              <input
+                name="name"
+                type="name"
+                className="input"
+                placeholder="Your Name"
+                required
+              />
+              {nameError && <p className="text-xs text-red-600">{nameError}</p>}
               {/* photo URL */}
               <label className="label">Photo URL</label>
               <input
+                name="photo"
                 type="photo url"
                 className="input"
                 placeholder="Your Photo URL"
+                required
               />
               {/* email */}
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                name="email"
+                type="email"
+                className="input"
+                placeholder="Email"
+                required
+              />
               {/* password */}
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input
+                name="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+                required
+              />
               <p className="text-center font-semibold pt-5">
                 Already Have An Account ?{" "}
                 <Link className="text-secondary" to="/auth/login">
                   Login
                 </Link>
               </p>
-              <button className="btn btn-neutral mt-4">Register</button>
+              <button type="submit" className="btn btn-neutral mt-4">
+                Register
+              </button>
             </fieldset>
-          </div>
+          </form>
         </div>
       </div>
     </div>
